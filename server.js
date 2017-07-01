5357969ac870babd9b42adb17f91b5f1bafbe2ae
Mike Semko <msemko@gmail.com>
2017-06-30 23:26:50 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,71 @@
+"use strict";
+
+var express = require("express");
+var app = express();
+var Gpio = require('onoff').Gpio;
+
+//this is mock for testing only
+
+// function Gpio(pin, initialState)  {
+// }
+
+// Gpio.prototype.write = function(data, cb) {
+//     //if (data === 0) throw new Error("test error");
+//     //nothing happens;
+//     setTimeout(cb);
+// };
+
+// Gpio.prototype.unexport = function() {
+//     //nothing happens
+// };
+
+var lock = new Gpio(14, 'high');
+
+var PORT = process.env.NODE_PORT || 3000;
+
+var BUZZER_INTERVAL = 1000;
+
+function activateBuzzer(cb) {
+    lock.write(0, cb);
+}
+
+function deactivateBuzzer(cb) {
+    lock.write(1, cb);
+}
+
+app.get("/opendoor", function(req, res, next){
+    activateBuzzer(function(err){
+        if (err) return next(err);
+        setTimeout(function(){
+            deactivateBuzzer(function(err){
+                if (err) return next(err);
+                return res.json({result: 'success'});
+            });
+        }, BUZZER_INTERVAL);
+    })
+});
+
+app.use(function(err, req, res, next) {
+    try {
+        deactivateBuzzer(function(){
+            console.log("Error detected - Buzzer deactivated");
+            console.log(err);
+            return res.json({result: 'error', error: err.message}); 
+        });
+    }
+    catch(err) {
+        console.log("Error detected - attempted to deactivate buzzer - failed!");
+        console.log(err);
+        return res.json({result: 'error', error: err.message}); 
+    }
+});
+
+app.listen(PORT, function() {
+    console.log("Server active on port " + PORT);
+});
+
+process.on("SIGINT", function(){
+    console.log("just exited");
+    lock.unexport();
+    process.exit();
+});
